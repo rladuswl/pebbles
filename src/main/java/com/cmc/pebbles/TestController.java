6dbcbdf0917abbac2682e160b6a1958dc63d7c9d
kyj <khkkgon0118@gmail.com>
2022-09-30 14:15:14 +0900

    beanstalk 배포 위한 설정 파일

---
+++ src/main/java/com/cmc/pebbles/TestController.java
@@ -0,0 +1,13 @@
+package com.cmc.pebbles;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() throws Exception{
+        return "test(테스트)";
+    }
+}
