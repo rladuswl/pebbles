600f938e6f7f41ce7df1b102d033048003317631
myeondii <khkkgon0118@gmail.com>
2022-10-25 02:18:18 +0900

    Delete JwtProperties.java

--- src/main/java/com/cmc/pebbles/config/jwt/JwtProperties.java
+++
@@ -1,8 +0,0 @@
-package com.cmc.pebbles.config.jwt;
-
-public interface JwtProperties {
-    String SECRET = "harupuppyPebbles";
-    int EXPIRATION_TIME =  60000*14400; // 10일
-    String TOKEN_PREFIX = "Bearer ";
-    String HEADER_STRING = "Authorization";
-}
